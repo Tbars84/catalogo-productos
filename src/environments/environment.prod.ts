c5a6489059b8601eb4670a238379d30469b2df9f
Camilo_Tabares <camilo.tabares.84@gmail.com>
2019-06-09 15:39:39 -0500

    preparando deploy para publicar en githu pages

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+  baseApi: `https://fvwzxk56cg.execute-api.us-east-1.amazonaws.com/mock`,
+  localApi: `http://localhost:3000`  
 };
